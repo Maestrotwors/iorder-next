1de47dd810b6619caf6e947bddfe1f3751cd9ab7
Rakzin Roman <maestrotwors@gmail.com>
2024-03-07 21:24:16 +0100

    fix

--- libs/frontend/page/customer/supplier-select/src/lib/frontend-page-customer-supplier-select/frontend-page-customer-supplier-select.component.ts
+++ libs/frontend/page/customer/supplier-select/src/lib/frontend-page-customer-supplier-select/frontend-page-customer-supplier-select.component.ts
@@ -1,10 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FrontendCustomerHeaderComponent } from '@iorder-next/frontend/core/customer/components/shared-customer/header';
 
 @Component({
   selector: 'io-customer-page-supplier-select',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FrontendCustomerHeaderComponent
+  ],
   templateUrl: './frontend-page-customer-supplier-select.component.html',
   styleUrl: './frontend-page-customer-supplier-select.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
